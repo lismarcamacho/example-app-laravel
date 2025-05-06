ceae9a90c3e63a866e38b01ec25d97299ee2f2fe
lismarcamacho <camacho.lismar55@gmail.com>
2025-05-05 22:17:32 -0400

    agregando otra ruta, organizando el orden de rutas

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,17 @@ Route::get('/cursos', function () {
     return"Bienvenido a la pagina de cursos";
 });
 
+Route::get('/cursos/create', function () {
+    return"En esta pagina podrás crear un curso";
+});
 
 ## definicion de la ruta contenido de curso
 
 Route::get('/cursos/{curso}', function ($curso) {
     return"Bienvenido al curso: $curso";
+});
+
+
+Route::get('/cursos/{curso}/{categoria}', function ($curso,$categoria) {
+    return"Bienvenido al curso: $curso de la categoria $categoria";
 });
