60951e3e3f9e4deb9962724af3a8a5960e11b2c3
lismarcamacho <camacho.lismar55@gmail.com>
2025-05-05 22:25:09 -0400

    agregando otra ruta, una forma de optimizar el codigo

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,31 @@ Route::get('/cursos/create', function () {
 
 ## definicion de la ruta contenido de curso
 
+/*
+
 Route::get('/cursos/{curso}', function ($curso) {
     return"Bienvenido al curso: $curso";
 });
 
+*/
+
 
 Route::get('/cursos/{curso}/{categoria}', function ($curso,$categoria) {
     return"Bienvenido al curso: $curso de la categoria $categoria";
+});
+
+## para no escribir dos veces estas rutas el 
+## ultimo codigo se puede reeescribir indicando ? (categoria coomo opcional)
+
+## inicializando la variable categoria e 
+## indicando que si tiene ul valor me devuelva las dos variables
+
+Route::get('/cursos/{curso}/{categoria?}', function ($curso,$categoria = null) {
+
+    if($categoria){
+    return "Bienvenido al curso: $curso de la categoria $categoria";
+    }else{
+        return "Bienvenido al curso: $curso
+
+    }
 });
