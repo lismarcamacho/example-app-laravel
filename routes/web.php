fdf8917c1cd523a907cc940efc2300482af03c18
lismarcamacho <camacho.lismar55@gmail.com>
2025-05-05 21:48:42 -0400

    agregando una ruta

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('/cursos', function () {
+    return"Bienvenido a la pagina de cursos";
+});
+
+
+## definicion de la ruta contenido de curso
+
+Route::get('/cursos/{curso}', function ($curso) {
+    return"Bienvenido al curso: $curso";
+});
